9f207477d6090ecc392eda978d6787313a1156a8
Willi Eßer <willi.esser@troublete.com>
2017-11-12 14:42:27 +0100

    (src)(doc) added interface doc for entity generator interface

--- src/Generator/Php/EntityGenerator/EntityGeneratorInterface.php
+++ src/Generator/Php/EntityGenerator/EntityGeneratorInterface.php
@@ -8,8 +8,27 @@ interface EntityGeneratorInterface
     const NAMESPACE_ENTITIES = 'Entities';
     const NAMESPACE_COLLECTIONS = 'Collections';
 
+    /**
+     * Method to process pattern defintions and create an entity
+     * structure based on processed patterns
+     * @param array $patterns
+     * @param string $targetNamespace
+     * @return null
+     */
     public function processPatterns(array $patterns, string $targetNamespace);
+
+    /**
+     * Method to retrieve created mapping entity structure
+     * @return array
+     */
     public function getGeneratedEntities() : array;
+
+    /**
+     * Method to retrieve created entity structure of
+     * entites for additional functionality
+     * @return array
+     */
     public function getEntities() : array;
+
     public function getCollections() : array;
 }
